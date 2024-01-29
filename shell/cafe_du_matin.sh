ba3ae5aae462795a6fe65f7b9601a3433b2e21cb
journaldethomas <51152315+journaldethomas@users.noreply.github.com>
2024-01-29 20:38:10 +0100

    Delete shell directory

--- shell/cafe_du_matin.sh
+++
@@ -1,5 +0,0 @@
-LOG_FILE=/config/shell/cafe.log
-touch $LOG_FILE
-echo "`date` : début du script" >> $LOG_FILE
-python ./switchbot-cmd.py E3:07:07:E3:A8:89 press
-echo "`date` : fin du script" >> $LOG_FILE
